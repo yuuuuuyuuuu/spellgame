82e943aace38f155b66d7934b9c3d4e53e2d5050
YuuuuuYuuuuu <yuu.android.dev@gmail.com>
2015-11-05 23:14:31 +0900

    :lipstick: cancel fullscreen

---
+++ SpellGame/Assets/Scripts/SGGameScreenSettingConroller/SGGameScreenSettingConroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SGGameScreenSettingConroller : MonoBehaviour {
+
+	public bool FullScreen = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Screen.fullScreen = this.FullScreen;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
